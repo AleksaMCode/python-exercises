16404b692d7086cb52673d3d246d8e03843e0742
Aleksa Majkic <aleksamcode@gmail.com>
2022-07-20 23:36:57 +0200

    Task 1 solution updated and test implemented.

---
+++ task_1/task1.py
@@ -0,0 +1,22 @@
+import math
+
+
+def quadratic_equation(a, b, c):
+    determinant = b * b - 4 * a * c
+
+    if a == b == c == 0:
+        return "Quadratic equation has infinitely many solutions."
+    elif a == 0:
+        return round(c / b, 2)
+    elif determinant < 0:
+        determinant *= -1
+        re = round(-b / (2 * a), 2)
+        im = round(math.sqrt(determinant) / (2 * a), 2)
+        print("Solution is: %.2f +/- %.2fi" % (re, im))
+
+        im = im if im > 0 else im * -1
+        return complex(re, im), complex(re, -im)
+    else:
+        return round((-b + math.sqrt(determinant)) / (2 * a), 2), round((-b - math.sqrt(determinant)) / (2 * a), 2)
+        # print("Solution is: %.2f or %.2f" % (
+        #     (-b + math.sqrt(determinant)) / (2 * a), (-b - math.sqrt(determinant)) / (2 * a)))
---
+++ task_1/test_task1.py
@@ -0,0 +1,22 @@
+import pytest
+from task_1 import task1
+
+test_complex_solutions = [
+    (5, 3, 2, (complex(-0.30, 0.56), complex(-0.30, -0.56))),
+    (65, 26, 269, (complex(-0.20, 2.02), complex(-0.20, -2.02))),
+    (-45, 13, -89, (complex(0.14, 1.40), complex(0.14, -1.40))),
+    (5, 65, -56, (0.81, -13.81)),
+    (45, 156, -5, (0.03, -3.50)),
+    (45, 13, -89, (1.27, -1.56)),
+    (0, 0, 0, "Quadratic equation has infinitely many solutions."),
+    (0, 2, 5, 2.50),
+    (5, 0, 5, (complex(0, 1), complex(0, -1))),
+    (5, 0, 5, (complex(0, 1), complex(0, -1))),
+    (45, 85, 0, (0, -1.89)),
+    (1, 1, 1, (complex(-0.50, 0.87), complex(-0.50, -0.87))),
+]
+
+
+@pytest.mark.parametrize("a, b, c, expected", test_complex_solutions)
+def test_quadratic_equation(a, b, c, expected):
+    assert task1.quadratic_equation(a, b, c) == expected
