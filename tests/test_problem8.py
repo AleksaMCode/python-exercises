daf846760198f6897c2a98b17bf497ed3bd82ea6
Aleksa Majkic <aleksamcode@gmail.com>
2022-07-22 18:27:32 +0200

    Tests for problem 8 added.

---
+++ tests/test_problem8.py
@@ -0,0 +1,58 @@
+import unittest
+
+from problem_8.exceptions import *
+from problem_8.problem8 import IotDevice
+from parameterized import parameterized
+
+unittest.TestLoader.sortTestMethodsUsing = None
+
+
+class TestUserIot(unittest.TestCase):
+    environment_variable = "DEVICES_PATH"
+    devices_test_data = [
+        [213, 121],
+        [123, 152],
+        [132131, 12],
+        [12321, 1211],
+        [1232, 85],
+        [2134, 75],
+        [12414, 347],
+    ]
+
+    email_test_data = [
+        "user1@gmail.com", "user2@gmail.com", "user3@gmail.com", "user4@gmail.com", "user5@gmail.com",
+        "user6@gmail.com", "user7@gmail.com"
+    ]
+
+    @parameterized.expand(devices_test_data)
+    def test1_1(self, sn, id):
+        """
+        Tests if it's possible to create devices from a list of correct data.
+        """
+        _ = IotDevice(sn, id)
+
+    def test1_2(self):
+        """
+        Tests if it's possible to write devices to a csv.
+        """
+        self.assertEqual(True, IotDevice.write(TestUserIot.environment_variable))
+
+    def test1_3(self):
+        """
+        Tests if it's possible to read devices from a csv.
+        """
+        self.assertEqual(True, IotDevice.read(TestUserIot.environment_variable))
+
+    def test1_4(self):
+        """
+        Tests if it's possible to read a file when environment variable is missing.
+        """
+        with self.assertRaises(EnvironmentVariableException):
+            _ = IotDevice.read("DEVICES__PATH")
+
+    def test1_5(self):
+        """
+        Test if it's possible to add device with an already existing sn.
+        """
+        with self.assertRaises(DuplicateDeviceException):
+            _ = IotDevice(TestUserIot.users_test_data[0][0], TestUserIot.users_test_data[0][1])
