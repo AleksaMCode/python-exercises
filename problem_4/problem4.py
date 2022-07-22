40e9066c8bc7ca179e75c248517381c8d6543efc
Aleksa Majkić <aleksamcode@gmail.com>
2022-07-22 08:23:44 +0200

    Code updated + strcmp function comments added.

--- problem_4/problem4.py
+++ problem_4/problem4.py
@@ -1,8 +1,16 @@
-def strcmp(first: str, second: str):
+def strcmp(first, second):
+    """
+    Compares two strings lexicographically.
+    :param first: String to be compared.
+    :param second: String to be compared.
+    :return: 0 if the contents of both strings are equal, <0 if the first character that does not match
+    has a greater value in ptr1 than in ptr2 and >0 if the first character that does not match has a lower
+    value in ptr1 than in ptr2
+    """
     if first == second:
         return 0
-    if len(first) != len(second):
-        return 0 if first == second else (-1 if len(first) > len(second) else 1)
+    elif len(first) != len(second):
+        return -1 if len(first) > len(second) else 1
     else:
         for i, j in zip(range(0, len(first)), range(0, len(second))):
             if first[i] != second[j]:
