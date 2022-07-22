03f68fc457be8972c1187a2d4a41d767699c70f8
Aleksa Majkić <aleksamcode@gmail.com>
2022-07-22 08:35:45 +0200

    Function comments added.

--- problem_5/problem5.py
+++ problem_5/problem5.py
@@ -1,4 +1,9 @@
-def rectangle_draw(size: int, symbol: str = "#"):
+def rectangle_draw(size: int, symbol="#"):
+    """
+    Prints a hollow rectangle pattern of symbols.
+    :param size: Size of the drawn rectangle.
+    :param symbol: Symbol used as a rectangle pattern.
+    """
     for i in range(0, size - 1):
         print()
         for j in range(0, size):
@@ -9,6 +14,11 @@ def rectangle_draw(size: int, symbol: str = "#"):
 
 
 def square_draw(size: int, symbol: str = "#"):
+    """
+    Prints a hollow square pattern of symbols.
+    :param size: Size of the drawn square.
+    :param symbol: Symbol used as a rectangle pattern.
+    """
     for i in range(0, size):
         print()
         for j in range(0, size):
