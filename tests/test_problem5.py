fcb156d85fe9670a4ea23d83323df05b95fc74d9
Aleksa Majkic <aleksamcode@gmail.com>
2022-07-22 19:42:43 +0200

    Tests of symbol '*' print added.

--- tests/test_problem5.py
+++ tests/test_problem5.py
@@ -1,8 +1,37 @@
 import pytest
-
+import sys
+from io import StringIO
 from problem_5 import problem5
 
 type_error_data = [5.05, "5", '5', (5, 56)]
+
+
+@pytest.mark.parametrize("print_value, size",
+                         [("\n****\n*  *\n****", 4),
+                          ("\n***\n***", 3),
+                          ])
+def test_rectangle_print(print_value: str, size: int):
+    captures_stdout = StringIO()  # Creates StringIO object
+    sys.stdout = captures_stdout  # Redirects stdout
+
+    problem5.rectangle_draw(size, '*')
+    sys.stdout = sys.__stdout__  # Reset redirect
+
+    assert captures_stdout.getvalue() == print_value
+
+
+@pytest.mark.parametrize("print_value, size",
+                         [("\n****\n*  *\n*  *\n****", 4),
+                          ("\n**\n**", 2),
+                          ])
+def test_square_print(print_value: str, size: int):
+    captures_stdout = StringIO()  # Creates StringIO object
+    sys.stdout = captures_stdout  # Redirects stdout
+
+    problem5.square_draw(size, '*')
+    sys.stdout = sys.__stdout__  # Reset redirect
+
+    assert captures_stdout.getvalue() == print_value
 
 
 @pytest.mark.parametrize("size", type_error_data)
